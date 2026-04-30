c1ece7c7856243faefa2383ae5786d52a1f6e7b6
SanskarCode1 <san.chimangaonkar@gmail.com>
2026-04-30 14:22:13 -0500

    dn

--- Sofit/ViewModels/SocialViewModel.cs
+++ Sofit/ViewModels/SocialViewModel.cs
@@ -11,5 +11,6 @@ public class SocialViewModel
     {
         Posts.Add(new Post { Author = "Alice", Content = "Great workout today!" });
         Posts.Add(new Post { Author = "Bob", Content = "New PR on squats." });
+        Posts.Add(new Post { Author = "Daisy", Content = "New PR on squat." });
     }
 }
